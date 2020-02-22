7e2c801afd60ab281be9fa942de0135ab13fa504
Arvind <akn0038@gmail.com>
2020-02-22 22:28:46 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,141 @@
+let express = require('express')
+let mongodb = require('mongodb')
+let sanitizeHTML = require("sanitize-html")
+
+let app = express()
+
+let db
+
+let port = process.env.PORT
+if(port == null || port == "")
+{
+  port=3000
+}
+app.use(express.static('public'))
+
+let connectionString = 'mongodb+srv://todoAppUser:2501@cluster0-cqvrb.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true} , function(err, client){
+db = client.db()
+app.listen(port)
+}) 
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+function passwordProtected(req,res,next)
+{
+  res.set('www-Authenticate','Basic realm="Simple ToDo App"')
+  //console.log(req.headers.authorization)
+  if(req.headers.authorization == "Basic QWtuMDAzODoyNTAx")
+  {
+    next()
+  }
+  else {
+    res.status(401).send("Authentication required")
+  }
+}
+app.use(passwordProtected)
+app.get('/', function(req,res){
+    db.collection('items').find().toArray(function(err,items){
+      res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Simple To-Do App</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+      <style>
+        body {
+          background-image: url("bg.jpg");
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-size: 100% 100%;
+          
+        }
+        #ourArea
+        {
+          margin-top:5%;
+          background: rgba(255,255,255,0.75);
+          border-radius: 30px;
+        }
+        
+      </style>
+      </head>
+    <body >
+      <div id="ourArea"class="container">
+        <h1  class="display-4 text-center py-1"><b><u>To-Do App</u></b></h1>
+        
+        <div class="jumbotron p-3 shadow-sm">
+          <form id="create-form" action="/create-item" method = "POST">
+            <div class="d-flex align-items-center">
+              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <button class="btn btn-primary">Add New Item</button>
+            </div>
+          </form>
+        </div>
+        
+        <ul id="item-list" class="list-group pb-5">
+        </ul>
+        
+
+      </div>
+      
+      <div id="ourArea"class="container">
+      <br>
+      <hr color="red" align="center" width="25%">
+
+	<center>
+		<b><i><u>contact Details</b></i></u><br>
+	</center>
+
+
+	<center>
+	
+			<i>
+				Email id:-<u>
+					<a href="https://mail.google.com/mail/?view=cm&fs=1&to=akn0038@gmail.com">
+				akn0038@gmail.com
+			</a>
+			</u>
+			</i>
+			<br>
+			<i>
+				Phone No:-<u>+91-8228059429</u>
+			</i>
+	
+	</center>
+
+<hr color="red" align="center" width="25%">
+<br>
+      </div>
+      <script>
+      let items = ${JSON.stringify(items)}
+      </script>
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+      <script src="browser.js"></script>
+    </body>
+    </html>`)
+    })
+    
+})
+
+app.post('/create-item',function(req,res){
+  let safeText = sanitizeHTML(req.body.text,{allowedTags: [], allowedAttributes:[]})
+    db.collection('items').insertOne({text: safeText},function(err,info){
+      res.json(info.ops[0])
+    })
+    
+})
+
+app.post('/update-item',function(req,res){
+  let safeText = sanitizeHTML(req.body.text,{allowedTags: [], allowedAttributes:[]})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)},{$set: {text: safeText} },function(){
+    res.send("Success")
+  })
+})
+app.post('/delete-item',function(req,res){
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function(){
+    res.send("success")
+  })
+})
